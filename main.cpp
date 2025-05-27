1493ccad55db6f641cc8a8b9bc2aa07a1a3ade82
Atarotesto <gomezsebastianemanuel1999@gmail.com>
2025-05-27 18:54:41 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int Seleccion;
+	
+	cout << "1 - Jugar" << endl;
+	cout << "2 - Estadistica" << endl;
+	cout << "3 - Creditos" << endl;
+	cout << " ------------------" << endl;
+	cout << "0 - Salir" << endl;
+	
+	switch(Seleccion){
+	case 1:
+		
+	case 2:
+		
+	case 3:
+		
+	case 0:	
+		
+	}
+	
+	
+	
+	return 0;
+}
+	
+	
